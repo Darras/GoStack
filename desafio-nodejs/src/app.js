4fbf6dbeb95842792216e645dc846548f5054273
Gabriel Darruiz <gabriel.darruiz@gmail.com>
2020-04-19 14:22:10 -0300

    correção

--- desafio-nodejs/src/app.js
+++ desafio-nodejs/src/app.js
@@ -15,29 +15,28 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  
-  var time = "";
-  console.time(time);
   var {title,url,techs} = request.body
   var repository = {id:uuid(), title:title,url:url,techs:techs,likes:0}
   repositories.push(repository)
   response.json(repository)
-  console.timeEnd(time);
-  console.log(time);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
   var repo = repositories.findIndex(repository => repository.id == request.params.id);
 
   if(repo >= 0){
-    repositories[repo] = {
-      id:request.params.id,
-      title:request.body.title,
-      url:request.body.url,
-      techs:request.body.techs,
-      likes:repositories[repo].likes
+    var test = {...repositories[repo], title:request.params.title}
+    const {title,url,techs} = request.body;
+
+
+    repositories[repo] = { 
+      ...repositories[repo],
+      title : title === undefined ? repositories[repo].title : title,
+      url: url === undefined ? repositories[repo].url : url,
+      techs: techs === undefined ? repositories[repo].techs : techs,
     }
-    return response.status(204).json(repositories[repo])
+    return response.json(repositories[repo])
   }else{
     return response.status(400).send()
   }
@@ -45,11 +44,10 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   var repo = repositories.findIndex(repository => repository.id == request.params.id);
-console.log(repo)
   if(repo >= 0){
     repositories.splice(repo,1);
     
-    return response.status(204).json({sucesso:"OK"})
+    return response.status(204).send()
   }else{
     return response.status(400).send()
   }
@@ -60,7 +58,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   if(repo !== undefined && repo >= 0){
     repositories[repo].likes ++;
-    return response.status(204);
+    return response.json({likes:repositories[repo].likes});
   }else{
     return response.status(400).send()
   }
